df558ff0a5e66960d627aa8a8dcc1056cf674e5d
prateekb1912 <prateekb1912@gmail.com>
2022-05-10 13:37:43 +0530

    Replace selenium driver with requests

--- weatherterm/core/request.py
+++ weatherterm/core/request.py
@@ -1,23 +1,16 @@
 import os
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+import requests
 
 class Request:
     def __init__(self, base_url):
-        self._phantomjs_path = os.path.join(os.curdir, 'phantomjs/bin/phantomjs')
         self._base_url = base_url
 
-        options = Options()
-        options.add_argument('-profile')
-        options.add_argument('/home/marvin/snap/firefox/common/.mozilla/firefox')
-        self._driver = webdriver.Firefox(options=options)
-    
     def fetch_data(self, forecast, area):
         url = self._base_url.format(forecast=forecast, area=area)
-        self._driver.get(url)
+        resp = requests.get(url)
 
-        if self._driver.title == '404 Not Found':
+        if resp.status_code == 404:
             error_msg = "Could not find the are you were looking for."
             raise Exception(error_msg)
         
-        return self._driver.page_source
+        return resp.text
--- weatherterm/parsers/weather_com_parser.py
+++ weatherterm/parsers/weather_com_parser.py
@@ -1,4 +1,5 @@
 import re
+from bs4 import BeautifulSoup
 
 from weatherterm.core import ForecastType, Request, Unit, UnitConverter
 
@@ -13,42 +14,13 @@ class WeatherComParser:
         }
         self._base_url = 'https://weather.com/weather/{forecast}/l/{area}'
         self._request = Request(self._base_url)
-
-        self._temp_regex = re.compile('([0-9]+)\D{,2}')
-        self._only_digits_regex = re.compile('[0-9]+')
         
         self._unit_converter = UnitConverter(Unit.FAHRENHEIT)
     
-    def _get_data(self, container, search_items):
-        scraped_data = {}
-
-        for k,v in search_items.items():
-            res = container.find(v, class_ = k)
-            data = None if res is None else res.get_text()
-
-            if data is not None:
-                scraped_data[k] = data
-        
-        return scraped_data
-
-    def _parse(self, container, criteria):
-        results = [self._get_data(item, criteria) for item in container.children]
-
-        return [res for res in results if res]
-
-    def _clear_str_number(self, str_number):
-        res = self._only_digits_regex.match(str_number)
-
-        return '--' if res is None else res.group()
-
-    def _get_additional_info(self, content):
-        data = tuple(item.td.span.get_text() for item in content.table.tbody.children)
-
-        return data[:2]
 
 
     def _today_forecast(self, args):
-        raise NotImplementedError()
+        raise NotImplementedError() 
     def _weekend_forecast(self, args):
         raise NotImplementedError()
     def _fivedays_forecast(self, args):
